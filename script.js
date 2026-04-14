6713a37bfeae57577a3ad493c13cc896570050c3
Asma Samadi <asmasamadi001@gmail.com>
2026-04-14 16:51:55 +0430

    Add js and style header

---
+++ script.js
@@ -0,0 +1,5 @@
+const toggle = document.getElementById("menu-toggle");
+const navList = document.getElementById("nav-list");
+toggle.addEventListener("click", () => {
+  navList.classList.toggle("active");
+});
