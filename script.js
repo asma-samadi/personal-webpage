9d5fbf275a73292456c503d1f80e5f9edf6a8fae
Asma Samadi <asmasamadi001@gmail.com>
2026-04-15 16:36:29 +0430

    Update CSS, and Js

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const navList = document.getElementById("nav-list");
 toggle.addEventListener("click", () => {
   navList.classList.toggle("active");
 });
+
+const links = document.querySelectorAll(".nav-link");
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    navList.classList.remove("active");
+  });
+});
